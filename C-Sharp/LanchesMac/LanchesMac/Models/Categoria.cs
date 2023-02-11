f189ab0b3d24807317a6c5f28a9eaa46c27cb923
RMV <rogerio.rmv@gmail.com>
2023-02-10 23:03:06 -0300

    Iniciada a criação do domínio com a inclusão das classes Categoria e Lanche

---
+++ C-Sharp/LanchesMac/LanchesMac/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        #region "Atributos da classe"
+        public int CategoriaId { get; set; }
+        public string? CategoriaNome { get; set; }
+        public string? Descricao { get; set; }
+        #endregion
+
+        #region "Atributos de relacionamento com Lanche"
+        public List<Lanche>? Lanches { get; set; } // Relacionamento com Lanche ( 1 para muitos )
+        #endregion
+    }
+}
---
+++ C-Sharp/LanchesMac/LanchesMac/Models/Lanche.cs
@@ -0,0 +1,22 @@
+﻿namespace LanchesMac.Models
+{
+    public class Lanche
+    {
+        #region "Atributos da classe"
+        public int LancheId { get; set; }
+        public string? Nome { get; set; }
+        public string? DescricaoCurta { get; set; }
+        public string? DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string? ImagemUrl { get; set; }
+        public string? ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+        #endregion
+
+        #region "Atributos de relacionamento com Categoria"
+        public int CategoriaId { get; set; } // Atributo chave-estrangeira
+        public virtual Categoria? Categoria { get; set; } // Relacionamento com Categoria ( 1 para 1 )
+        #endregion
+    }
+}
