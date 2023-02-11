79cc32c42793984ea36b93570454651d6805fd9e
RMV <rogerio.rmv@gmail.com>
2023-02-11 17:46:19 -0300

    Realizada a anotação (Annotation) dos atritutos das entidades Categoria e Lanche

--- C-Sharp/LanchesMac/LanchesMac/Models/Categoria.cs
+++ C-Sharp/LanchesMac/LanchesMac/Models/Categoria.cs
@@ -1,10 +1,36 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    /**
+     * Não há necessidade de anotar o nome da tabela neste ponto. 
+     * Estamos usando as convenções do framework. Pela conversão, 
+     * o próprio framework nomearia a tabela conforme definição 
+     * realizada no mapemento da tabela na classe AddDbContext 
+     * ***/
+    [Table("Categorias")] 
     public class Categoria
     {
         #region "Atributos da classe"
+        /**
+         * Pelos mesmos motivos da anotação da tabela, esta anotação (Key)
+         * se torna desnecessária. Ou seja, pela convenção e definição
+         * da entidade, já seria suficiente para que o framework
+         * criasse a tabela e seu atributos de maneira adequada
+         * ***/
+
+        [Key]
         public int CategoriaId { get; set; }
+
+        [StringLength(100, ErrorMessage = "O tamanho máximo é de 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [Display(Name = "Nome")]
         public string? CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanhno máximo é de 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Descrição")]
         public string? Descricao { get; set; }
         #endregion
 
--- C-Sharp/LanchesMac/LanchesMac/Models/Lanche.cs
+++ C-Sharp/LanchesMac/LanchesMac/Models/Lanche.cs
@@ -1,16 +1,48 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
         #region "Atributos da classe"
+        [Key]
         public int LancheId { get; set; }
+
+        [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+        [Display(Name = "Nome do Lanche")]
+        [StringLength(80, MinimumLength = 3, ErrorMessage = "O {0} deve ter no mínimo {2} e no máximo {1} caracteres")]
         public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
+        [Display(Name = "Descrição do Lanche")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "A descrição do lanche deve ter no mínimo {2} e no máximo {1} caracteres")]
         public string? DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "A descrição detalhada do lanche deve ser informada")]
+        [Display(Name = "Descrição detalhada")]
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "A descrição detalhada do lanche deve ter no mínimo {2} e no máximo {1} caracteres")]
         public string? DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage = "O preço do lanche deve ser informado")]
+        [Display(Name = "Preço do lanche")]
+        [Column(TypeName = "decimal(5,2)")]
+        [Range(0.01, 999.99, ErrorMessage = "O preço do lanhce deve estar entre R$ 0,01 e R$ 999,99")]
         public decimal Preco { get; set; }
+
+        [Display(Name = "Caminho Imagem Normal")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter o máximo {1} caracteres")]
         public string? ImagemUrl { get; set; }
+
+        [Display(Name = "Caminho Imagem Miniatura")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter o máximo {1} caracteres")]
         public string? ImagemThumbnailUrl { get; set; }
+
+        [Display(Name = "Preferido?")]
         public bool IsLanchePreferido { get; set; }
+
+        [Display(Name = "Estoque")]
         public bool EmEstoque { get; set; }
         #endregion
 
