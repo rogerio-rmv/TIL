b2602a1e5df1d3cfe7e8b9f79e6bf7ca6080072b
RMV <rogerio.rmv@gmail.com>
2023-02-11 20:35:09 -0300

    Criação de uma migration para popular a tabela de categorias

---
+++ C-Sharp/LanchesMac/LanchesMac/Migrations/20230211232746_PopularCategorias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Categorias(CategoriaNome, Descricao " +
+                "VALUES ('Normal', 'Lanche feito com ingredientes normais'");
+
+            migrationBuilder.Sql("insert into Categorias(CategoriaNome, Descricao " +
+                "VALUES ('Natural', 'Lanche feito com ingredientes integrais e naturais'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Categorias");
+        }
+    }
+}
