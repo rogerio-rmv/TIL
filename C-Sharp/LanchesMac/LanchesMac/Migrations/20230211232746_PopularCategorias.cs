e94edae68938315dbc8abcccdb4773a7d4442ff7
RMV <rogerio.rmv@gmail.com>
2023-02-12 16:04:36 -0300

    Foram realizadas correções na instrução insert no migration para PopularCategorias e adiciondo um novo migration para popular lanches.

--- C-Sharp/LanchesMac/LanchesMac/Migrations/20230211232746_PopularCategorias.cs
+++ C-Sharp/LanchesMac/LanchesMac/Migrations/20230211232746_PopularCategorias.cs
@@ -10,11 +10,11 @@ namespace LanchesMac.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("insert into Categorias(CategoriaNome, Descricao " +
-                "VALUES ('Normal', 'Lanche feito com ingredientes normais'");
+            migrationBuilder.Sql("insert into Categorias(CategoriaNome, Descricao) " +
+                " values ('Normal', 'Lanche feito com ingredientes normais') ");
 
-            migrationBuilder.Sql("insert into Categorias(CategoriaNome, Descricao " +
-                "VALUES ('Natural', 'Lanche feito com ingredientes integrais e naturais'");
+            migrationBuilder.Sql("insert into Categorias(CategoriaNome, Descricao) " +
+                " values ('Natural', 'Lanche feito com ingredientes integrais e naturais') ");
         }
 
         /// <inheritdoc />
