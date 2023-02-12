cf258a7e91d55c557931a0523de692d629f7bc76
RMV <rogerio.rmv@gmail.com>
2023-02-12 17:56:09 -0300

    * Implementação da interface e da classe concreta para a lanche

---
+++ C-Sharp/LanchesMac/LanchesMac/Repositories/LancheRepository.cs
@@ -0,0 +1,31 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LancheRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches
+                                                      .Include(lanches => lanches.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches
+                                                                .Where(lanches => lanches.IsLanchePreferido == true)
+                                                                .Include(lanches => lanches.Categoria);
+
+        public Lanche GetLancheById(int LancheId)
+        {
+               return _context.Lanches
+                              .Include(lanches => lanches.Categoria)
+                              .FirstOrDefault(lanches => lanches.LancheId == LancheId, new Lanche());
+        }
+    }
+}
