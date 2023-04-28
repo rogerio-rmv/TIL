b492d6713cc63a9dd6497f4999bcf7e3aee6af61
RMV <rogerio.rmv@gmail.com>
2023-04-27 23:12:42 -0300

    1) Criação das classes Categoria e Produto

--- C-Sharp/APICatalogo/APICatalogo/Models/Produto.cs
+++ C-Sharp/APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,11 +1,23 @@
-﻿namespace APICatalogo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
         public int ProdutoId { get; set; }
+        [Required]
+        [StringLength(80)]
         public string? Nome { get; set; }
+        [Required]
+        [StringLength(300)]
         public string? Descricao { get; set; }
+        [Column(TypeName = "decimal(10,2)")]
         public decimal Preco { get; set; }
+        [Required]
+        [StringLength(300)]
         public string? IMagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set;}
